78f08263483a76c9506d4b3dde5ed3691b15d096
Rick Wouters <r.a.d.wouters@student.tue.nl>
2017-06-28 14:35:04 +0200

    Initial commit

---
+++ kissingGraph.js
@@ -0,0 +1,31 @@
+$.get("nodes.json", function (data) {
+    var cy = cytoscape({
+        container: document.getElementById('graph-div'),
+        elements: data,
+
+        style: [ // the stylesheet for the graph
+            {
+                selector: 'node',
+                style: {
+                    'background-color': '#666',
+                    'label': 'data(id)'
+                }
+            },
+
+            {
+                selector: 'edge',
+                style: {
+                    'width': 3,
+                    'line-color': '#ccc',
+                    'target-arrow-color': '#ccc',
+                    'target-arrow-shape': 'triangle'
+                }
+            }
+        ],
+
+        layout: {
+            name: 'grid',
+            rows: 1
+        }
+    });
+});
