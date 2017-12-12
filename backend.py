76696b57a28d2eede66f79a7ea52b003bc249281
Wouters <r.a.d.wouters@student.tue.nl>
2017-12-12 17:20:16 +0100

    Now works with logins!

--- backend.py
+++ backend.py
@@ -2,11 +2,26 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required, current_user
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lqgyyqfljqbqxd:51fc0180ccd4c866433e83da29d4bfae51670195b46b9b1937e75c8cfd4d1aa9@ec2-54-247-177-33.eu-west-1.compute.amazonaws.com:5432/debauug427oq59'
 app.config['SECRET_KEY'] = "JE_MOEDER"
+app.config['SECURITY_PASSWORD_SALT'] = "jemoeder"
+app.config['SECURITY_REGISTERABLE'] = True
 db = SQLAlchemy(app)
 admin = Admin(app, name="Zoengraaf", template_mode="bootstrap3")
+
+roles_users = db.Table('roles_users',
+        db.Column('visitor_id', db.Integer(), db.ForeignKey('visitor.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
 
 class Interaction(db.Model):
     __tablename__ = "interactions"
@@ -28,26 +43,41 @@ class User(db.Model):
         return "<User id:{0} name: {1}>".format(self.id, self.name)
 
 
+class Visitor(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+user_datastore = SQLAlchemyUserDatastore(db, Visitor, Role)
+security = Security(app, user_datastore)
+
+
 class KissingGraphModelView(ModelView):
     def is_accessible(self):
-        for user in ALLOWED_USERS:
-            if request.args.get("password") == user["password"] and request.args.get("username") == user["username"]:
-                return True
-        return False
+        return "Admin" in current_user.roles
 
     def inaccessible_callback(self, name, **kwargs):
         return redirect("/")
 
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Interaction, db.session))
+
+admin.add_view(KissingGraphModelView(User, db.session))
+admin.add_view(KissingGraphModelView(Interaction, db.session))
+admin.add_view(KissingGraphModelView(Visitor, db.session))
+admin.add_view(KissingGraphModelView(Role, db.session))
 
 
 @app.route('/')
+@login_required
 def hello_world():
     return render_template("index.html")
 
 
 @app.route("/getInteractions")
+@login_required
 def get_kisses():
     users_array = []
     for user in User.query.all():
@@ -63,4 +93,4 @@ def get_kisses():
 
 if __name__ == '__main__':
     db.create_all()
-    app.run("0.0.0.0")
+    app.run("0.0.0.0", port=5001)
